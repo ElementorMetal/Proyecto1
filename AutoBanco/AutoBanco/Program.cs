122a276391407c2e9049282283dcf72726f5d577
EduHerrera0220 <71846639+EduHerrera0220@users.noreply.github.com>
2020-09-24 18:52:32 -0600

    Cambio

--- AutoBanco/AutoBanco/Program.cs
+++ AutoBanco/AutoBanco/Program.cs
@@ -11,6 +11,7 @@ namespace AutoBanco
         static void Main(string[] args)
         {
             Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Putos");
 
             Console.ReadKey();
         }
