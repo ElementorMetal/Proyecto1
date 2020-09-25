fdc72c17cdb5c8b9e748105e10f7c17b9f895f37
ElementorMetal <71747248+ElementorMetal@users.noreply.github.com>
2020-09-25 00:07:33 -0600

    versión1

--- AutoBanco/AutoBanco/Program.cs
+++ AutoBanco/AutoBanco/Program.cs
@@ -10,10 +10,6 @@ namespace AutoBanco
     {
         static void Main(string[] args)
         {
-            //Mostrar fecha actual
-            DateTime thisDay = DateTime.Today;
-            Console.WriteLine(thisDay.ToString("d"));
-
             //Declarando usuarios
             Usuario ElmaMado = new Usuario();
             ElmaMado.nombre = "Elma Mado";
@@ -27,13 +23,82 @@ namespace AutoBanco
             Eddie.pin = 9988;
             Eddie.saldo = 5000;
 
+            Usuario Roberto = new Usuario();
+            Roberto.nombre = "Roberto Peláez";
+            Roberto.numCuenta = "313199";
+            Roberto.pin = 0319;
+            Roberto.saldo = 4500;
+
+            int opcionMenu = 0;
+
+            do
+            {
+                //Mostrar fecha actual
+                DateTime thisDay = DateTime.Today;
+                Console.WriteLine(thisDay.ToString("d"));
+
+                Console.WriteLine("Ingrese 1 si desea ir al área administrativa \nIngrese 2 si desea ir a estaciones de servicio \nIngrese 3 si desea ir a reportes \n Ingrese 4 si desea salir");
+                opcionMenu = int.Parse(Console.ReadLine());
+
+                switch(opcionMenu)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese 1 si se encuentra en la estación 1 o 2 si está en la estación 2");
+                        int sedeServ = 0, sede1 = 0, sede2 = 0;
+                        string numCuenta = "";
+                        int pin = 0;
+                        sedeServ = int.Parse(Console.ReadLine());
+
+                        if(sedeServ == 1)
+                        {
+                            Console.WriteLine("Ingrese su número de cuenta");
+                            numCuenta = Console.ReadLine();
+                            Console.WriteLine("Ingrese su PIN");
+                            pin = int.Parse(Console.ReadLine());
+                            if (numCuenta == ElmaMado.numCuenta && pin == ElmaMado.pin)
+                            {
+                                int menuServicio = 0;
+                                Console.WriteLine("Ingrese 1 para Compra/Venta de dólares \nIngrese 2 para retiros \nIngrese para depósitos \n Ingrese 4 para transferencias");
+                                menuServicio = int.Parse(Console.ReadLine());
+
+                                switch (menuServicio)
+                                {
+                                    case 1: //Compra/Venta de dólares
+                                        break;
+                                    case 2: //Retiros
+                                        break;
+                                    case 3: //Depósito
+                                        break;
+                                    case 4: //Transferencias
+                                        break;
+                                }
+                            }
+                        }
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default: Console.WriteLine("Datos incorrectos");
+                        break;
+                }
+            }
+            while (opcionMenu != 4);
             Console.ReadKey();
         }
         //Métodos
-        static string Depositos(Usuario u1, int monto)
+        static int Depositos(Usuario u1, int monto)
         {
-
+            u1.saldo = u1.saldo + monto;
+            return u1.saldo;
         }
-        
+        static int Transferencias(Usuario u1, Usuario u2, int monto)
+        {
+            u1.saldo = u1.saldo - monto;
+            u2.saldo = u2.saldo + monto;
+            return monto;
+        }
     }
 }
